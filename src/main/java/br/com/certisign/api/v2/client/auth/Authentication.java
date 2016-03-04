c08f102b42438dcfd43a2d548d6ca7048c419e78
Gule Ifanger <valmir.ifanger@sensedia.com>
2016-03-04 15:32:42 -0300

    removing

--- src/main/java/br/com/certisign/api/v2/client/auth/Authentication.java
+++
@@ -1,8 +0,0 @@
-package br.gov.ima.api.v1.client.auth;
-
-import java.util.Map;
-
-public interface Authentication {
-	/** Apply authentication settings to header and query params. */
-	void applyToParams(Map<String, String> queryParams, Map<String, String> headerParams);
-}
